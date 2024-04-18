ed4804ffda58b050c603b8bb13c74480f7a1d422
Ariel De Santo <adesanto@vdscorp.com.ar>
2024-04-18 20:23:56 -0300

    Añadir modelo de datos de restaurante y ruta para obtener restaurantes en app.py

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+import pandas as pd
 
 app = Flask(__name__)
+uri = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+db = SQLAlchemy(app)
+
+
+class Restaurant(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    rating = db.Column(db.Integer)
+    name = db.Column(db.String)
+    site = db.Column(db.String)
+    email = db.Column(db.String)
+    phone = db.Column(db.String)
+    street = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+
+
+@app.before_first_request
+def setup():
+    db.create_all()
+    if Restaurant.query.first() is None:
+        data = pd.read_csv('restaurants.csv')
+        for _, row in data.iterrows():
+            restaurant = Restaurant(**row)
+            db.session.add(restaurant)
+        db.session.commit()
 
 @app.route('/')
 def index():
     return 'Hello, World! Testing Melp API'
+
+@app.route('/restaurants')
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return {'restaurants': [{'name': restaurant.name, 'rating': restaurant.rating} for restaurant in restaurants]}
