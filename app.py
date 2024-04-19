b8eaa70e357cffd261f1aaea991aedb2c55ee5dc
Ariel De Santo <adesanto@vdscorp.com.ar>
2024-04-18 21:44:51 -0300

    Corregir URI de la base de datos en app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 
 app = Flask(__name__)
 uri = os.getenv('DATABASE_URL')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 db = SQLAlchemy(app)
 
